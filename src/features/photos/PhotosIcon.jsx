87e44fd087d728fe311bec896e6ea969b734c07d
aditya <v.aditya.ramadhan@banksaqu.co.id>
2026-08-05 22:22:52 +0700

    feat: polish Photos gallery experience

--- src/features/photos/PhotosIcon.jsx
+++ src/features/photos/PhotosIcon.jsx
@@ -1,13 +1,4 @@
-const PETALS = [
-  { angle: 0, from: "#ff9f0a", to: "#ffd60a" },
-  { angle: 45, from: "#ffd60a", to: "#a7f3d0" },
-  { angle: 90, from: "#a7f3d0", to: "#34c759" },
-  { angle: 135, from: "#34c759", to: "#0a84ff" },
-  { angle: 180, from: "#0a84ff", to: "#bf5af2" },
-  { angle: 225, from: "#bf5af2", to: "#ff2d55" },
-  { angle: 270, from: "#ff2d55", to: "#ff453a" },
-  { angle: 315, from: "#ff453a", to: "#ff9f0a" },
-];
+import { PHOTOS_ICON_PETALS } from "./photosIconModel";
 
 export function PhotosIcon({ size = 58 }) {
   return (
@@ -17,18 +8,19 @@ export function PhotosIcon({ size = 58 }) {
       aria-hidden="true"
     >
       <span className="photos-icon__surface">
-        {PETALS.map(({ angle, from, to }) => (
-          <span
-            className="photos-icon__petal"
-            key={angle}
-            style={{
-              "--photos-icon-angle": `${angle}deg`,
-              "--photos-icon-from": from,
-              "--photos-icon-to": to,
-            }}
-          />
-        ))}
-        <span className="photos-icon__center" />
+        <span className="photos-icon__pinwheel">
+          {PHOTOS_ICON_PETALS.map(({ angle, from, to }) => (
+            <span
+              className="photos-icon__petal"
+              key={angle}
+              style={{
+                "--photos-icon-angle": `${angle}deg`,
+                "--photos-icon-from": from,
+                "--photos-icon-to": to,
+              }}
+            />
+          ))}
+        </span>
       </span>
     </span>
   );
---
+++ src/features/photos/photosIconModel.js
@@ -0,0 +1,10 @@
+export const PHOTOS_ICON_PETALS = [
+  { angle: 0, from: "#ff9f0a", to: "#ff6a00" },
+  { angle: 45, from: "#ffe14d", to: "#f5c400" },
+  { angle: 90, from: "#c9e265", to: "#7ac70c" },
+  { angle: 135, from: "#4fce7c", to: "#0f9d58" },
+  { angle: 180, from: "#4fc3f7", to: "#1078d2" },
+  { angle: 225, from: "#a89bea", to: "#7b3fd0" },
+  { angle: 270, from: "#ff6fd8", to: "#e0189c" },
+  { angle: 315, from: "#ff7a7a", to: "#ec2b3f" },
+];
