95ccc790a8db31a01f64c541d914d8bca889810e
aditya11201 <aditya11201@users.noreply.github.com>
2026-08-17 04:37:54 +0000

    deploy: 763f6a477546098d0d918e1c9195e63746392c11

--- assets/icons-CTR-OYej.js
+++
@@ -1,1 +0,0 @@
-import"./finder-W_Z6Z1y9.js";import"./terminal-DnNL5vCe.js";
--- assets/notes-BxdAWVTh.js
+++ assets/notes-BxdAWVTh.js
@@ -1,4 +1,4 @@
-import{r as n,W as w,m as e}from"./finder-W_Z6Z1y9.js";function g(){const{onClose:i,onMinimize:r,onZoom:l,onTitleMouseDown:d}=n.useContext(w),m=[{id:1,title:"Histories",content:`Love arrives
+import{r as n,W as w,m as e}from"./finder-BYGefIiS.js";function g(){const{onClose:i,onMinimize:r,onZoom:l,onTitleMouseDown:d}=n.useContext(w),m=[{id:1,title:"Histories",content:`Love arrives
 and in its train come ecstasies
 old memories of pleasure
 ancient histories of pain.
